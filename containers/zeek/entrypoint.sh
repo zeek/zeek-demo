9b702811142f27c79023d99b88b56c68cc2cd194
Arne Welzel <arne.welzel@corelight.com>
2023-01-11 21:48:51 +0100

    zeek/container: Add socat for listening on 4789/6081
    If we don't do this, then the container sends out ICMP port unreachable
    messages back to the host/docker-proxy and we essentially see spurious
    gaps. This took a while :-(

--- containers/zeek/entrypoint.sh
+++ containers/zeek/entrypoint.sh
@@ -8,6 +8,13 @@ BPF_FILTER=${BPF_FILTER:-udp port (6081 or 4789)}
 
 zeek --version
 
+# Listen on UDP 4789 and UDP 6081 so that the container does
+# not send out ICMP port unreachable responses for incoming
+# mirrored traffic. We can probably have this cheaper with
+# a custom daemon, but hey, this does it for now.
+socat udp-recv:4789 /dev/null,ignoreeof &
+socat udp-recv:6081 /dev/null,ignoreeof &
+
 # TODO, Runs single process Zeek, no cluster.
 exec zeek \
     -i "af_packet::${INTERFACE}" \
